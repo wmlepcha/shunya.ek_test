3b757f6b32c4c1f3d2e86afa366cab9ef1b7ef1c
wilburlepcha <wmlepcha15@gmail.com>
2020-11-12 14:20:53 +0530

    successfully grabbed HTML code

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
 async function getWebsite() {
-  let url = document.getElementById("sitelink").value;// DOM FOR ACCESING USER ENTERED URL
+  let url = document.getElementById("sitelink").value; // DOM FOR ACCESING USER ENTERED URL
   let urlFinal = "https://cors-anywhere.herokuapp.com/" + url; //SOLUTION FOR CORS ERROR
   fetch(urlFinal) // FETCH FUNCTION FOR GRABBING THE CONTENT
-    .then((response) => response.text()) // CONVERTING IT INTO TEXT
-    .then((response) => {
-      var mainContainer = document.getElementById("myData");// WHERE TO DISPLAY
+    .then(function (response) {
+      convertedText = response.text();
+      parser = new DOMParser();
+      getNode = parser.parseFromString(convertedText, "text/xml");
+      return getNode; // CONVERTING IT INTO TEXT
+    })
+    .then(function (getNode) {
+      var mainContainer = document.getElementById("myData"); // WHERE TO DISPLAY
       var div = document.createElement("div");
-      div.innerHTML = response;//DISPLAY CONTENT HERE
+
+      div.innerHTML = "Grabbed The HTML code Check The Console"; //DISPLAY CONTENT HERE
       mainContainer.appendChild(div);
       console.log("ITS WORKING");
+      console.log(getNode);
     })
     .catch(function () {
       // This is where you run code if the server returns any errors
@@ -16,4 +23,30 @@ async function getWebsite() {
     });
 }
 
+async function getJson() {
+  // Test with an element.
+
+  let url = document.getElementById("sitelink").value; // DOM FOR ACCESING USER ENTERED URL
+  let urlFinal = "https://cors-anywhere.herokuapp.com/" + url; //SOLUTION FOR CORS ERROR
+  fetch(urlFinal) // FETCH FUNCTION FOR GRABBING THE CONTENT
+    .then(function (response) {
+      var initElement = document.getElementsByTagName("html")[0];
+      var json = mapDOM(initElement, true);
+      console.log(json);
+    })
+    .then(function (myJSON) {
+      var mainContainer = document.getElementById("myData"); // WHERE TO DISPLAY
+      var div = document.createElement("div");
+
+      div.innerHTML = "Grabbed The JSON code Check The Console"; //DISPLAY CONTENT HERE
+      mainContainer.appendChild(div);
+      console.log("ITS WORKING");
+      console.log(myJSON);
+    })
+    .catch(function () {
+      // This is where you run code if the server returns any errors
+      console.log("NOT WORKING");
+    });
+}
 getWebsite();
+getJson();
